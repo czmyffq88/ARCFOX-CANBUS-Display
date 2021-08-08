c42731106e03433a3bdeab8a1ec5b8f05d490abd
qwec01 <45970146+qwec01@users.noreply.github.com>
2021-08-08 11:29:14 +0800

    Add files via upload

---
+++ ARCFOX_CAN_Display_HUD/ARCFOX_CAN_Display_HUD.ino
@@ -0,0 +1,279 @@
+/*
+   ACAN2515全局变量使用61字节，默认接收缓存32条512字节，默认发送缓存16条256字节，一个CAN总线使用849字节
+   PRI CAN接收缓存32*16=512字节，发送缓存0，变量61字节，共578字节
+   SEC CAN接收缓存4*16=64字节，发送缓存0，变量61字节，共125字节
+*/
+#include <Arduino.h>
+#include <EEPROM.h>
+#include <SPI.h>
+#include <ACAN2515.h>
+#include <TM1638.h>
+#define SEC_CS 8
+#define PRI_CS 9
+#define SEC_INT 2  //副CAN线，仪表板通信，包含HVAC温度etc
+#define PRI_INT 3  //主CAN线，包括单体电压、电池温度、电机控制etc
+#define debug 0    //不接串口屏时设为1，否则为0
+#define mirror 1    //HUD是否镜像显示
+#define DIO 4
+#define CLK 6
+#define STB 7
+//-----------------------------------------------------//
+//                     根据车型修改数据                 //
+//-----------------------------------------------------//
+#define KM      180   //满电续航，km， 根据车型修改
+#define ENERGY  23.2  //标称电量，kWh，根据车型修改
+#define VMax    360   //充满电时的最高电压，根据车型修改
+#define VMin    280   //快没电时的最低电压，根据车型修改
+
+//---------------数据表格-------------------
+//  车型    KM    ENERGY    VMax    VMin
+//  180     180   23.2      360     280
+//  210
+//  260
+//  301
+//  316
+//  CS15
+//----------------------------------------------------//
+
+
+//flag bit:     7         6         5         4         3     2     1     0
+//         HVACstat  dcdcCurrent  rstkmall  TrckFrce    Ri
+
+char MaxBatProbTemp, MinBatProbTemp;
+float energy;
+int power, powert = -1, spd = 0, spdt, consume, consumet, Current;
+unsigned int SOC, SOCt, SOC100, SOC_BMS, SOCt_BMS, SOC100_BMS, consumeavg, Efficiency, count185t;
+unsigned int meter_per_SOC, meter_per_SOC_BMS, used_SOC, used_SOC_BMS, ChgMinute;
+byte charging = 0,SlowChg, FastChg,HUDdot=0, hour, hourt,minute, minutet,  i, ex, spgnow, spg, bn, color = 15;
+byte MaxVoltNum, MinVoltNum,  MaxBatProbTempNum, MinBatProbTempNum, HVACstat;
+byte  km0, BVoltagebuf[24][8],  odo[3], eepromWrote;
+byte fBatTemp = 0, fBatNum = 0, fspd = 0, flag = 0, dispatched = 0;
+char Temp[8], Tempt[8], BTemp[16];
+unsigned int Voltagebox, powerbox, TractionForceBox, refreshinterval = 333, BVoltage[100], Ri,  MaxVolt, MinVolt;
+unsigned int kmremaining, kmall, BMSkmremaining, BMSkmall;
+unsigned int dcdcCurrent, ChgVin, ChgIin, ChgPin;
+int motorspd = 0, Voltage, Voltaget, Voltagei, MotorTorque, brightorg;
+byte bright, HUDbright;
+unsigned long runtime, lastruntime, refresh, brightruntime, ODO, ODOt, ODObegin, ODO100, t, count185 = 0, ODObeginForKmallCalc;
+int BrightFilterBuf[11] = {1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024}, TractionForce;
+static uint32_t XTAL = 16UL * 1000UL * 1000UL; //晶振16M
+
+ACAN2515 PRI (PRI_CS, SPI, PRI_INT);
+ACAN2515 SEC (SEC_CS, SPI, SEC_INT);
+TM1638Display HUD(DIO, CLK, STB);     //HUD声明
+
+//------------------------------------------------------------------MCP2515定义
+ACAN2515Mask rxm1 = standard2515Mask (0x7FF , 0, 0);
+ACAN2515Settings settings (XTAL, 500UL * 1000UL);
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.setTimeout(20);
+  SPI.begin();
+  delay(100);
+  //------------------------------------------------↓初始化SEC CAN线-----------------------
+  {
+    //    Serial.println(F("Configuring SEC CANBUS"));
+    settings.mRequestedMode = ACAN2515Settings::NormalMode;
+    settings.mReceiveBufferSize = 8;
+    settings.mTransmitBuffer0Size = 0;
+    ACAN2515Mask rxm0 = standard2515Mask (0x7DF , 0, 0);
+    const ACAN2515AcceptanceFilter filters [] = {
+      {standard2515Filter (0x307 , 0, 0), RCV0},//ID307&327，压缩机&SOC 10+10/s
+      {standard2515Filter (0x7ff , 0, 0), RCV1},
+      {standard2515Filter (0x3c1 , 0, 0), RCV2},//温度 10/s
+      {standard2515Filter (0x2a2 , 0, 0), RCV3},//温度 10/s
+      {standard2515Filter (0x2b1 , 0, 0), RCV4},//电压电流充电状态10/s
+      {standard2515Filter (0x380 , 0, 0), RCV5} //ODO 1/s
+    };
+    unsigned int errorCode = SEC.begin (settings, [] { SEC.isr (); }, rxm0, rxm1, filters, 6); //isr中断服务程序在源码的ACAN2515.cpp里
+#if debug
+    if (errorCode == 0)
+      Serial.println(F("SEC CAN Success"));
+    else
+    {
+      Serial.print(F("SEC Init Fail, ErrCode=0x"));
+      Serial.println(errorCode, HEX);
+    }
+#endif
+  }
+  //------------------------------------------------↑初始化SEC CAN线
+
+  //------------------------------------------------↓初始化PRI CAN线
+  {
+    //    Serial.println(F("Configuring PRI CANBUS"));
+    settings.mReceiveBufferSize = 26;
+    settings.mTransmitBuffer0Size = 0;
+    ACAN2515Mask rxm0 = standard2515Mask (0x7CF , 0, 0);
+    const ACAN2515AcceptanceFilter filters [] = {
+      {standard2515Filter (0x615 , 0, 0), RCV0},//ID 615&625 最高最低温度&最高最低单体电压 10+10/s
+      {standard2515Filter (0x231 , 0, 0), RCV1},//DCDC电压电流 10/s
+      {standard2515Filter (0x153 , 0, 0), RCV2},//电机速度、扭矩 100/s
+      {standard2515Filter (0x7ff , 0, 0), RCV3},//523,续航
+      {standard2515Filter (0x185 , 0, 0), RCV4},//电压电流SOC 100/s
+      {standard2515Filter (0x191 , 0, 0), RCV5} //HVAC状态 100/s
+    };
+    //一秒330帧数据，3ms一帧  //一个中断140us
+    unsigned int errorCode = PRI.begin (settings, [] { PRI.isr (); }, rxm0, rxm1, filters, 6); //664us, isr中断服务程序在源码的ACAN2515.cpp里
+#if debug
+    if (errorCode == 0)
+      Serial.println(F("PRI CAN Success"));
+    else
+    {
+      Serial.print(F("PRI Init Fail, ErrCode=0x"));
+      Serial.println(errorCode, HEX);
+    }
+#endif
+  }
+  //------------------------------------------------↑初始化PRI CAN线------------------------
+  EEPROM.get(0, ODObegin);  //清零时的总里程
+  EEPROM.get(4, energy);    //清零后的总耗电量
+  EEPROM.get(12, ODO100);   //重算续航时的总里程
+  EEPROM.get(16, SOC100);   //重算续航时的SOC
+  EEPROM.get(18, SOC100_BMS);
+  EEPROM.get(24, ODObeginForKmallCalc); //4 bytes
+  EEPROM.get(28, used_SOC);
+  EEPROM.get(30, used_SOC_BMS);
+  delay(50);
+  while (Serial.read() >= 0); //清空串口缓存
+  SPG_TPN(1, 2);
+#if !debug
+  while (!Serial.find("OK"));
+#else
+  delay(500);
+#endif
+  //--------------------------------------------显示EEPROM值
+  Serial.print(F("DS24(0,180,'Used SOC_BMS: ")); Serial.print(used_SOC_BMS/10.0); Serial.print("%',15,0);");
+  Serial.print(F("DS24(0,210,'Used SOC:     ")); Serial.print(used_SOC/10.0); Serial.print("%',15,0);");
+  Serial.print(F("DS24(0,240,'energy:       ")); Serial.print(energy/1000.0); Serial.print("kWh',15,0);");
+  Serial.print(F("DS24(0,270,'ODO begin:    ")); Serial.print(ODObeginForKmallCalc/10.0); Serial.println("km',15,0);");
+  HUD.rst();//上电初始化
+  for (byte i = 0; i <= 1; i++)//全显测试
+  {
+    HUD.SendCommand(0x40);//数据命令设置，写数据到显存，地址自动增加
+    HUD.SendAddr(0xC0);//第一个显存地址
+    for (byte j = 0; j <= 15; j++)
+    {
+      HUD.SendData(0xFF);
+    }
+    HUD.SendCommand(0x8F);
+    delay(500);
+    HUD.SendCommand(0x40);//数据命令设置，写数据到显存，地址自动增加
+    HUD.SendAddr(0xC0);//第一个显存地址
+    for (byte j = 0; j <= 15; j++)
+    {
+      HUD.SendData(0);
+    }
+    HUD.SendCommand(0x8F);
+    delay(500);
+  }
+  delay(1000);
+  while (Serial.read() >= 0);
+  SPG_TPN(2, 2);
+  spg = 2;
+  spgnow = 2;
+#if !debug
+  while (!Serial.find("OK"));
+#else
+  delay(500);
+#endif
+  flag += 0xF0;
+  for (i = 0; i <= 7; i++)
+    Tempt[i] = 127;
+}
+
+
+void loop()
+{
+  dispatch();
+  runtime = millis();
+  if (spg == 2 || spg == 4) refreshinterval = 333;
+  if (spg == 3) refreshinterval = 1000;
+  if (spg == 5) refreshinterval = 5000;
+  if (runtime - brightruntime >= 200)
+  {
+    bright = brightFilter();
+    if (motorspd > -3000)
+      spd = abs(motorspd) / 7.3170; //←换胎的话修改这个数值
+    //------------------------------------------------------------------//
+    //                            根据个人喜好修改                       //
+    //------------------------------------------------------------------//
+    /*  HUD可以以镜像方式显示2个4位数，范围为-1999~9999，可以选择小数位数，推荐为1位
+         HUD投影在风档上时，右侧为第一个数
+         下面3行程序的第一行 byte dot[2] = {1, 1}; 大括号内分别为HUD上两个数值的小数位数，左边是第一个数的
+         第二行 float aa[2] = {spd / 10.0, Current / 10.0}; 大括号内分别个HUD显示的数值，
+         注意本程序中有部分数值是以10倍存储的，显示正常值必须除以10.0。（“.0绝对不能省略”）
+         如果不想显示第2个数，设置第2个数为-2000，且小数位数为0
+         任何数值小于-1999都将不显示，或显示错误（忽略小数点）
+         第三行 HUD.Refresh(aa, dot, HUDbright, 1, 1);
+         小括号内前2个变量不要修改，第三个变量是亮度，第四个是显示开/关，第5个是镜像，1是，0否
+    */
+    byte dot[2] = {1, 1}; //小数位数
+    float aa[2] = {spd / 10.0, Current / 10.0}; //要显示的数
+    if (charging ==0)
+      HUD.Refresh(aa, dot, HUDbright, 1, 1);
+    //------------------------------------------------------------------//
+    //外部电流表输出
+    Ameter();
+    brightruntime = runtime;
+  }
+  if (Serial.available() > 0)//---------------------------------按钮
+  {
+    if (Serial.read() == '[')
+    {
+      bn = Serial.parseInt();
+      if (bn != 0)
+      {
+        if (bn < 10 && bn != 4 && bn != 6)
+        {
+          spg = bn;
+          changeFilt();
+          //printUSART();
+          lastruntime = millis();
+          runtime = lastruntime;
+        }
+        if (bn == 4)  //能耗清零
+        {
+          ODObegin = ODO;
+          energy = 0;
+          EEPROM.put(0, ODObegin);
+          EEPROM.put(4, energy);
+        }
+        if (bn == 6)  //重算续航
+        {
+          SOC100 = SOC;
+          ODO100 = ODO;
+          ODObeginForKmallCalc = ODO;
+          SOC100_BMS = SOC_BMS;
+          used_SOC = 0;
+          used_SOC_BMS = 0;
+          EEPROM.put(12, ODO);  //4 bytes
+          EEPROM.put(16, SOC);
+          EEPROM.put(18, SOC_BMS);
+          kmall = KM;
+          kmremaining = KM * (SOC / 100.0);
+          BMSkmall = KM;
+          BMSkmremaining = KM * (SOC_BMS / 100.0);
+          EEPROM.put(24, ODObeginForKmallCalc);
+          EEPROM.put(28, used_SOC);
+          EEPROM.put(30, used_SOC_BMS);
+          if ((flag & 0x20) == 0)
+            flag += 0x20;
+        }
+      }
+    }
+    while (Serial.read() >= 0);
+  }
+  if (runtime - lastruntime > refreshinterval)
+  {
+    if (charging ==1)
+    {
+      HUD.ShowTime(hour*60+minute,HUDbright,HUDdot);
+      HUDdot=!HUDdot;
+    }
+    printUSART();
+    lastruntime = runtime;
+  }
+
+}
---
+++ ARCFOX_CAN_Display_HUD/printUSART.ino
@@ -0,0 +1,537 @@
+void printUSART()
+{
+  char str[7];
+  while (Serial.read() >= 0);
+  //----------------------------------判断充电或运行-------------------------
+  byte j = 0, h = 0, l = 0;
+  if (spg == 2 || spg == 3)
+  {
+    //if (debug == 1) km0 = 129;
+//    if (km0 >= 128 && Current <= -10)
+    if (SlowChg == 1)
+    {
+      charging = 1;
+      spg = 3;
+    }
+    else
+    {
+      charging = 0;
+      spg = 2;
+    }
+
+  }
+  if (spgnow != spg)
+  {
+    SPG_TPN(spg, 2);
+#if !debug
+    while (!Serial.find("OK"));
+    dispatch();
+#else
+    delay(300);
+    dispatch();
+#endif
+    spgnow = spg;
+    setZero();
+    changeFilt();
+    lastruntime = millis();
+    runtime = lastruntime;
+  }
+
+
+
+  //-----------------------------------------------------------------------//
+  //-------------------------------↓计算↓----------------------------------//
+  //-----------------------------------------------------------------------//
+  power = (Voltage / 10.0) * ( Current / 10.0) / 100.0; //10倍,kW
+  if (abs(Current) < 5) //算内阻
+    Voltagei = Voltage;
+  if (abs(Current) >= 500 && Voltagei != 0)
+  {
+    Ri = 1000 * ((float)(Voltagei - Voltage) / Current);
+    Voltagei = 0;
+    if ((flag & 0x08) == 0)
+      flag += 0x08;
+  }
+  if (charging != 1)
+  {
+    //energy += power * (0.01 * count185) / 36.0; //单位为kW·h * 1000, energy为int时使用
+    energy += power / 10.0 * (0.01 * count185) / 3.6; //单位为W·h, energy为float时使用
+    count185 = 0;
+    //-------------------------------------------------------------计算使用的SOC
+    if (SOCt_BMS != 0)
+      used_SOC_BMS += SOCt_BMS - SOC_BMS;
+    if (SOCt != 0)
+      used_SOC += SOCt - SOC;
+  }
+  //-------------------------------------------------------------计算平均续航
+  if (used_SOC != 0)
+  {
+    meter_per_SOC = ((ODO - ODObeginForKmallCalc) / (used_SOC / 10.0)) * 100.0;
+    kmall = meter_per_SOC / 10.0;
+  }
+  else
+  {
+    kmall = KM;
+  }
+  if (used_SOC_BMS != 0)
+  {
+    meter_per_SOC_BMS = ((ODO - ODObeginForKmallCalc) / (used_SOC_BMS / 10.0)) * 100.0;
+    BMSkmall = meter_per_SOC_BMS / 10.0;
+  }
+  else
+  {
+    BMSkmall = KM;
+  }
+  kmremaining = kmall * (SOC / 1000.0);
+  BMSkmremaining = BMSkmall * (SOC_BMS / 1000.0);
+
+
+
+  //电压条算法：(BOX的y2-1)-((电压-最小电压)/(最大电压-最小电压))*(条高度-2)
+  TractionForce = MotorTorque * 2.683732; //牵引力 = 转矩 / 10 * 齿轮比 / 车轮半径
+  //170Nm时，轮上转矩1348.17，牵引力4562.34，轮径591mm
+  Voltagebox = 312 - ((Voltage/10.0 - VMin) / (VMax-VMin)) * 201;
+  powerbox = 312 - (abs(power) / 700.0) * 201;
+  TractionForceBox = 312 - (abs(TractionForce) / 4600.0) * 201;
+  if (motorspd > -3000)
+    spd = abs(motorspd) / 7.3170;//←换胎的话修改这个数值
+  ODO = (odo[0] * 65536 + (unsigned long)odo[1] * 256 + odo[2]);
+  if (charging != 1)
+  {
+    if (spd >= 50 &&  TractionForce > 0)
+      Efficiency = (TractionForce * (spd / 3.6) / power) * 10.0;
+    else
+      Efficiency = 0;
+  }
+  else
+  {
+    ChgPin = ChgVin * ChgIin / 10.0; ///瓦特
+    Efficiency = ((Voltage / 10.0) * abs(Current / 10.0)) / ChgPin * 1000.0;
+    if (Efficiency >= 999)  Efficiency = 999;
+  }
+  if (charging == 1);
+  {
+    if (FastChg == 1)
+    {
+      float timeremaining;
+      timeremaining = (1.0 - (SOC_BMS / 1000.0)) * ENERGY / abs(power / 10.0);
+      hour = (int)timeremaining;
+      minute = (timeremaining - (float)hour) * 60;
+    }
+    else
+    {
+      hour = ChgMinute / 60.0;
+      minute = ChgMinute % 60;
+    }
+  }
+  if (ODO != ODObegin)  //算平均电耗
+  {
+    consumeavg = (energy / 1000.0) / ((ODO - ODObegin) / 10.0) * 10000.0; //10倍
+  }
+  if (SOC == 0)
+    kmremaining = 0;
+  if (SOC_BMS == 0)
+    BMSkmremaining = 0;
+  if (spd > 50 && Current > 0)
+    consume = power * (1000.0 / spd);
+  else
+    consume = -1;
+
+  //-----------------------------------------------------------------------//
+  //-----------------------------↑计算↑----------------------------------//
+  //-----------------------------------------------------------------------//
+
+
+  //---电流----//
+  if (spg == 2 || spg == 3)
+  {
+    //-------------------------------------屏显亮度信息------------------------//
+    //    Serial.print(F("DS24(0,0,'AnalogRead:"));                           //
+    //    Serial.print(brightorg);                                            //
+    //    Serial.print("   ',15);");                                          //
+    //    Serial.print(F("DS24(0,24,'Brightness:"));                          //
+    //    Serial.print(bright);                                               //
+    //    comma(',');                                                         //
+    //    Serial.print(HUDbright);                                            //
+    //    Serial.print("   ',15);");                                          //
+    //------------------------------------------------------------------------//
+    color = 15;
+    if (abs(Current) >= 1000)
+      dtostrf(abs(Current) / 10.0, 3, 0, str);
+    else
+      dtostrf(abs(Current) / 10.0, 2, 1, str);
+    if (Current >= 1000) color = 1;
+    if (Current >= 0 && Current < 1000) color = 15;
+    if (Current < 0) color = 2;
+    LABL(48, 29, 65, 114, str, color, 2);
+    Current = 0; //显示完电流就清零，debug用
+    ex++;
+    //----------功率------------
+    if (power != powert) {
+      dtostrf(abs(power) / 10.0, 2, 1, str);
+      LABL(48, 29, 114, 114, str, color, 2);
+
+      if (power < 0 && powert > 0) {
+        color = 2;
+        LABL(24, 0, 75, 27, "-", color, 1);
+        LABL(24, 0, 123, 27, "-", color, 1);
+        ex++;
+      }
+      if (power >= 0 && powert < 0) {
+        color = 15;
+        LABL(24, 0, 75, 27, "+", color, 1);
+        LABL(24, 0, 123, 27, "+", color, 1);
+        ex++;
+      }
+      ex += 4; powert = power;                          //ex max = 7
+    }
+
+
+
+    //-------------------------电量
+    if ((ODO != ODOt) || (SOC != SOCt) || ((flag & 0x20) == 0x20))
+    {
+      if (SOC == 1000)
+        dtostrf(SOC / 10.0, 3, 0, str);
+      else
+        dtostrf(SOC / 10.0, 2, 1, str);
+      if (SOC >= 500) color = 2;
+      else if (SOC >= 250) color = 4;
+      else color = 1;
+      LABL(32, 19, 207, 74 , str, color, 2);
+      ex++;
+      dtostrf(kmremaining, 3, 0, str);//剩余续航
+      LABL(32, 174, 207, 225, str, 15, 1);
+      dtostrf(kmall, 3, 0, str);//总续航
+      LABL(32, 91, 207, 146, str, 15, 1);
+      ex += 2;
+      SOCt = SOC;
+    }
+    if ((SOC_BMS != SOCt_BMS) || ((flag & 0x20) == 0x20))
+    {
+
+      if (SOC_BMS == 1000)
+        dtostrf(SOC_BMS / 10.0, 3, 0, str);
+      else
+        dtostrf(SOC_BMS / 10.0, 2, 1, str);
+      if (SOC_BMS >= 500) color = 2;
+      else if (SOC_BMS >= 250) color = 4;
+      else color = 1;
+      LABL(32, 19, 239, 74 , str, color, 2);
+      ex++;
+      if ((SOC_BMS != SOCt_BMS) || (ODO != ODOt) || ((flag & 0x20) == 0x20))
+      {
+        dtostrf(BMSkmremaining, 3, 0, str);//剩余续航
+        LABL(32, 174, 239, 225, str, 15, 1);
+        dtostrf(BMSkmall, 3, 0, str);//总续航
+        LABL(32, 91, 239, 146, str, 15, 1);
+        ex += 2;
+      }
+      if ((flag & 0x20) == 0x20)
+        flag -= 0x20;
+      SOCt_BMS = SOC_BMS;
+    }
+
+
+    if ((flag & 0x20) == 0x20)
+      flag -= 0x20;
+    execute();
+
+    if (charging != 1)
+    {
+
+    }
+    //------------------电压---------------------
+    if (Voltage != Voltaget) {
+      dtostrf(Voltage / 10.0, 3, 1, str);
+      LABL(48, 141, 64, 244, str, 15, 2);
+      dtostrf(MaxVolt, 4, 0, str);
+      LABL(24, 11, 271, 71, str, 15, 2);
+      dtostrf(MinVolt, 4, 0, str);
+      LABL(24, 11, 295, 71, str, 15, 2);
+      dtostrf(MaxVoltNum, 2, 0, str);
+      LABL(24, 122, 271, 147, str, 15, 2);
+      dtostrf(MinVoltNum, 2, 0, str);
+      LABL(24, 122, 295, 147, str, 15, 2);
+      if (Voltaget < Voltage)
+      {
+        BOXF(405, Voltagebox, 438, 312, 4);
+      }
+      else
+      {
+        BOXF(405, 111, 438, Voltagebox - 1, 0);
+      }
+      ex += 7; Voltaget = Voltage;              //ex max = 20
+    }
+    execute();
+    //---------------电池温度
+    if (fBatTemp == 1)
+    {
+      dtostrf(MaxBatProbTemp, 2, 0, str);
+      LABL(24, 176, 272, 213, str, 5, 1);
+      dtostrf(MinBatProbTemp, 2, 0, str);
+      LABL(24, 176, 296, 213, str, 5, 1);
+      ex += 2;
+      fBatTemp = 0;
+    }
+    if (fBatNum == 1)
+    {
+      dtostrf(MaxBatProbTempNum, 2, 0, str);
+      LABL(24, 267, 271, 292, str, 5, 1);
+      dtostrf(MinBatProbTempNum, 2, 0, str);
+      LABL(24, 267, 295, 292, str, 5, 1);
+      ex += 2;
+      fBatNum = 0;
+    }
+    execute();
+  }
+
+  if (spg == 2) //-----------------------------------------------仅行驶时显示
+  {
+//----------------空调状态-------------
+    if ((flag & 0x80) == 0x80)
+    {
+      dtostrf(HVACstat, 2, 0, str);
+      LABL ( 32, 285, 48, 328, str, 15, 1);
+      flag -= 0x80;
+      ex += 1;
+    }
+    if ((flag & 0x08) == 0x08)
+    {
+      itoa(Ri, str, 10);  //内阻
+      LABL(32, 177, 174, 226, str, 15, 2);
+      ex += 1;
+      flag -= 0x08;
+    }
+    //---------------------牵引力----------------
+    if ((flag & 0x10) == 0x10)
+    {
+      if (TractionForce >= 0)
+        dtostrf(TractionForce, 4, 0, str);
+      else
+        dtostrf(TractionForce, 5, 0, str);
+      LABL(32, 19, 174, 100, str, 15, 2);
+
+      if (TractionForce >= 3000) color = 1;
+      else if (TractionForce >= 0) color = 3;
+      else color = 2;
+      BOXF(443, TractionForceBox, 476, 312, color);
+      BOXF(443, 111, 476, TractionForceBox - 1, 0);
+      ex += 4;
+      flag -= 0x10;
+      //dispatch();
+    }
+    execute();
+    //----------------------------充电机-----------------------
+    if ((flag & 0x40) == 0x40)
+    {
+      dtostrf(dcdcCurrent / 10.0, 2, 1, str);
+      LABL(24, 269, 165, 320, str, 15, 1);
+      dtostrf(((dcdcCurrent / 10.0) * 14.5), 3, 0, str);
+      LABL(24, 334, 165, 378, str, 15, 1);
+      flag -= 0x40;
+      ex += 2;
+    }
+    //--------------------------能耗-----------------------
+    if (consume != consumet) {
+      if (consume == -1) strcpy(str, "--.-");
+      else if (consume >= 1000) strcpy(str, "99.9");
+      else dtostrf(consume / 10.0, 2, 1, str);
+      LABL(48, 141, 114, 224, str, 15, 2);
+      ex++;
+      consumet = consume;
+    }
+    if (ODO != ODOt)  //上电后平均能耗
+    {
+      dtostrf(consumeavg / 100.0, 2, 2, str);
+      LABL(32, 293, 131, 370,  str, 15, 2);
+      Serial.print("DS32(350,0,'"); dispatch();
+      Serial.print(ODO / 10); Serial.print('.'); dispatch();
+      Serial.print(ODO % 10); Serial.print("',15,0);"); dispatch();
+      ODOt = ODO;
+      ex += 2;                                 //ex max = 19;
+    }
+    dtostrf(energy / 1000.0, 2, 2, str);
+    LABL(32, 293, 98, 370, str, 15, 2);
+
+    ex ++;
+
+    dtostrf(spd / 10.0, 3, 1, str); //速度
+    if (spd >= 1000) color = 1;
+    else color = 15;
+    if (fspd == 1)
+    {
+      LABL(48, 376, 42, 479, str, color, 2);
+      fspd = 0;
+      ex++;                                   //ex max = 20
+    }
+    execute();
+    //-------------------------效率---------------------
+    dtostrf(Efficiency / 100.0, 2, 1, str);
+    LABL(32, 219, 5, 277, str, 15, 2);
+    ex++;
+    //---------------------------温度---------------------
+    for (j = 3; j <= 7; j++)
+    {
+      if (Temp[j] != Tempt[j])  //j=0-7:3=逆变器，4=蒸发器，5=制热液，6=冷却液，7=回风口
+      {
+        dtostrf(Temp[j], 2, 0, str);
+        LABL(24, 345, 198 + (j - 3) * 24, 382, str, 5, 1);
+        ex++;
+        Tempt[j] = Temp[j];
+        ex++;
+        execute();
+      }
+
+    }
+
+
+
+    if (ODO != ODObegin && spd == 0) //写EEPROM
+    {
+      unsigned long ODOtemp;
+      EEPROM.get(8, ODOtemp);
+      if (ODOtemp != ODO)
+      {
+        EEPROM.put(8, ODO);
+        EEPROM.put(4, energy);
+        eepromWrote++;
+        unsigned int SOCtemp;
+        EEPROM.get(28, SOCtemp);
+        if (SOCtemp != used_SOC)
+        {
+          EEPROM.put(28, used_SOC);
+          eepromWrote++;
+        }
+        EEPROM.get(30, SOCtemp);
+        if (SOCtemp != used_SOC_BMS)
+        {
+          EEPROM.put(30, used_SOC_BMS);
+          eepromWrote++;
+        }
+        Serial.print(F("DS16(320,185,'")); Serial.print(eepromWrote); Serial.print("',15,0);");
+      }
+    }
+    //    Serial.print();
+  }
+
+  if (spg == 3)//------------------------------------------------仅充电时显示
+  {
+    if ((hour != hourt) || (minute != minutet)) {
+      Serial.print(F("LABL(48,194,114,300,'"));
+      Serial.print(hour);
+      Serial.print(':');
+      if (minute < 10) Serial.print('0');
+      Serial.print(minute);
+      Serial.print("',15,2);");
+      hourt = hour;
+      minutet = minute;
+      ex += 2;
+    }
+    //市电电压
+    dtostrf(ChgVin, 3, 0, str);
+    LABL(24, 345, 40, 390, str, 15, 2);
+    //市电电流
+    dtostrf(ChgIin / 10.0, 2, 1, str);
+    LABL(24, 410, 40, 460, str, 15, 2);
+    //市电功率
+    dtostrf(ChgPin, 4, 0, str);
+    LABL(24, 335, 65, 390, str, 15, 2);
+    //充电效率
+    dtostrf(Efficiency / 10.0, 2, 1, str);
+    LABL(32, 219, 5, 277, str, 15, 2);
+    execute();
+    //电池温度
+    
+    for (j = 0; j <= 15; j++)
+    {
+      CELS(16,0,j,BTemp[j],0);
+    }
+    ex += 1;
+    execute();
+    h = 0;
+    for (j = 0; j <= 7; j++)
+    {
+      if (j == 1 || j == 2 || j == 3) continue;
+      if ( Temp[j] != Tempt[j])
+      {
+        dtostrf(Temp[j], 2, 0, str);
+        LABL(24, 345, 198 + h * 24, 382, str, 5, 1);
+        ex++;
+        Tempt[j] = Temp[j];
+      }
+      h++;
+    }
+  }
+
+#if 1
+  if (spg == 5) //------------------------------------BMS页面----------占用634字节
+  {
+    //CELS(m,h,l,'显示内容',c,bc,ali);用m点阵字体在h行l列的单元格中,用c号颜色字,bc号颜色背景,显示相应内容
+    //,ali：显示方案 0-居左  1-居中  2-居右
+    byte k;
+    for (k = 0; k <= 23; k++)
+    {
+      for (i = 0; i <= 7; i = i + 2)
+      {
+        BVoltage[j] = BVoltagebuf[k][i] * 256.0 + BVoltagebuf[k][i + 1];
+        if (BVoltage[j] > 5000)
+          BVoltage[j]=0;
+        j++;
+      }
+    }
+    j = 0;
+    for (h = 0; h <= 9; h++)
+    {
+      for (l = 0; l <= 9; l++)
+      {
+//        if (j == 100) break;
+        CELS(24,l, h, BVoltage[j], 0);
+        j++;
+        if (j == 25 || j == 50 || j == 75)
+        {
+          Serial.println();
+#if !debug
+          while (!Serial.find("OK"));
+#else
+          delay(75);
+#endif
+        }
+      }
+    }
+    Serial.println();
+#if !debug
+          while (!Serial.find("OK"));
+#else
+          delay(75);
+#endif
+    Serial.print(F("DS16(0,295,'T: "));
+    j = 0;
+    for (j = 0; j <= 15; j++)
+    {
+      if (BTemp[j]==-40) break;
+      Serial.print(BTemp[j], DEC);
+      comma(' ');
+    }
+    Serial.print("  ',15,0);");
+
+    CELS(24,MaxVoltNum % 10, MaxVoltNum / 10, MaxVolt, 1);
+    CELS(24,MinVoltNum % 10, MinVoltNum / 10, MinVolt, 3);
+//    Serial.println(); delay(100);
+    //ex = 1;
+  }
+  //------------------------------------↑BMS页面----------
+#endif
+
+  Serial.print("SEBL("); Serial.print(bright); Serial.print(",1);");  //SEBL(n,t); t =1 时仅调节背光不存储（可省）
+  Serial.println();
+  ex = 0;
+#if !debug
+  while (!Serial.find("OK"));
+  dispatch();
+#else
+  delay(75);
+  dispatch();
+#endif
+
+}
